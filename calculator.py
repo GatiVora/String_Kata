17c6512fe496a95226a5545ecd8cb542f9496e95
Gati Vora <gmvora.2021@gmail.com>
2024-08-01 18:02:32 +0530

    handling , and /n delimeters

--- calculator.py
+++ calculator.py
@@ -3,4 +3,25 @@ import re
 def add(numbers):
     if not numbers:
         return 0
-    return 0 
+
+    delimiter = ','
+    numbers = re.split(rf'[{delimiter}\n]', numbers)
+    total = 0
+    negatives = []
+    for number in numbers:
+        if number:
+            try:
+                num = int(number)
+                if num < 0:
+                    negatives.append(num)
+                else:
+                    total += num
+            except Exception as e:
+                raise ValueError("Kindly provide a string of valid integers separated using a single delimiter")
+        else:
+            raise ValueError("Invalid input")
+
+    if negatives:
+        raise ValueError(f"negative numbers not allowed {','.join(map(str, negatives))}")
+
+    return total
